4f0e88092e0cfaf567f6782b94f893ab98acb2cb
MrMoshkovitz <gal@gmail.com>
2020-08-24 17:54:11 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+import logo from './logo.svg';
+// import Ticket from './components/Ticket'
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      moshko al titiash
+    </main>
+  );
+}
+
+export default App;
