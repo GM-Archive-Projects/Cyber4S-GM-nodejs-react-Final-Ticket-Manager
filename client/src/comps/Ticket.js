0c439808d7169d2625200d8031d94df3cfd5f5d3
MrMoshkovitz <gal@gmail.com>
2020-08-25 03:12:23 +0300

    Seperate Labels

--- client/src/comps/Ticket.js
+++ client/src/comps/Ticket.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react'
 
 const Ticket = (props) => {
+    const temp = props.labels;
+const temp2 = 
+    console.log(temp)
 
-    const getLabels = props.labels.map((label) => <p className={label}>{label}</p>)
-    console.log(getLabels)
     return (
         <div className="ticket">
             <h1>{JSON.stringify(props.title)}</h1>
@@ -11,10 +12,18 @@ const Ticket = (props) => {
             {props.content}
             </div>
             <br></br>
+            <p>{JSON.stringify((props.labels))}</p>
+            <p>{props.labels && props.labels.map((label) => <p className={label}>{label}</p>)}</p>
+            {/* <p>{labels.from((label)=> <button>{label}</button>)}</p> */}
         {/* {JSON.stringify(props.ticki)} */}
-            {JSON.stringify(props.labels)}
+            {/* {props.labels.map((label) => 
+            && props.labels !== undefined)
+            
+            <p className={label}>{label}</p>)} */}
         </div>
     );
 }
+
+// {JSON.stringify(props.labels)}
 
 export default Ticket;
