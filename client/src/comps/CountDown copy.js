3446eb2a492cc2e2f49e637e24030f7d9d000b6a
MrMoshkovitz <gal@gmail.com>
2020-09-06 10:08:32 +0300

    Commiting Fixing linrar Value Damagged Undone for comleted

---
+++ client/src/comps/CountDown copy.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ProgressBar } from 'react-bootstrap';
+
+const progressInstance = ({ seconds }) => {
+  const animatedSeconds = seconds;
+
+  return (
+    <ProgressBar animated now={animatedSeconds * 20} variant="danger" label={`${seconds} Seonds Left Before Ticket Mark as Completed`} />
+  );
+};
+export default progressInstance;
